2f705e786f8417052e20e8fe5ef78ba9a69fcecc
Arif Hossain <hossaininfo@ymail.com>
2023-02-12 18:34:48 +0100

    created hero addHeroMenu

--- src/main/java/org/example/services/HeroManagementService.java
+++ src/main/java/org/example/services/HeroManagementService.java
@@ -3,15 +3,58 @@ package org.example.services;
 import org.example.pojos.heroes.*;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class HeroManagementService {
 
-    public static ArrayList<Hero> heroes = new ArrayList<>();
-    static Hero mage = new Mage("Erik");
-    static Hero ranger = new Ranger("Arif");
-    static Hero rogue = new Rogue("Melvin");
-    static Hero warrior = new Warrior("Rubin");
+    public static Scanner sc = new Scanner(System.in);
+    static ArrayList<Hero> heroes = new ArrayList<>();
+
+    public static void addHeroesMenu() {
+        System.out.println("What kind of hero you want to see?");
+        System.out.println("=====================");
+        System.out.println("1. Mage: ");
+        System.out.println("2. Ranger: ");
+        System.out.println("3. Rogue: ");
+        System.out.println("4. Warrior: ");
+        System.out.println("5. All Hero");
+        System.out.println("0. Choose 0 to EXIT!!");
+        System.out.println("=====================");
+        System.out.print("\nMake a choice: ");
+        int choice = readIntNumber();
+        switch (choice) {
+            case 1:
+                createMage();
+                break;
+            case 2:
+                createRanger();
+                break;
+
+            case 3:
+                createRogue();
+                break;
+
+            case 4:
+                createWarrior();
+                break;
+            case 5:
+                createMage();
+                createRanger();
+                createRogue();
+                createWarrior();
+                break;
+            case 0:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Wrong choice!!");
+        }
+
+    }
+
     public static void createMage() {
+        Hero mage = new Mage("Erik");
         mage.setHeroClass("Mage");
         mage.setLevel(2);
 
@@ -25,10 +68,12 @@ public class HeroManagementService {
             mage.setEquipment(mage.equip());
         }
         heroes.add(mage);
+        System.out.println("You just added a Mage " + mage);
     }
 
 
     public static void createRanger() {
+        Hero ranger = new Ranger("Arif");
         ranger.setHeroClass("Ranger");
         ranger.setLevel(2);
 
@@ -42,10 +87,13 @@ public class HeroManagementService {
             ranger.setEquipment(ranger.equip());
         }
         heroes.add(ranger);
+        System.out.println("You just added a Ranger " + ranger);
+        System.out.println("=====================");
 
     }
 
-    public static void createRogue(){
+    public static void createRogue() {
+        Hero rogue = new Rogue("Melvin");
         rogue.setHeroClass("Rogue");
         rogue.setLevel(2);
 
@@ -59,10 +107,13 @@ public class HeroManagementService {
             rogue.setEquipment(rogue.equip());
         }
         heroes.add(rogue);
+        System.out.println("You just added Rogue " + rogue);
+        System.out.println("=====================");
 
     }
 
     public static void createWarrior() {
+        Hero warrior = new Warrior("Rubin");
         warrior.setHeroClass("Warrior");
         warrior.setLevel(5);
 
@@ -76,17 +127,39 @@ public class HeroManagementService {
             warrior.setEquipment(warrior.equip());
         }
         heroes.add(warrior);
+        System.out.println("You just added Warrior " + warrior);
+        System.out.println("=====================");
 
     }
-
 
 
     public static void display() {
         for (Hero hero : heroes) {
             System.out.println(hero);
         }
+        System.out.println("=====================");
     }
 
+
+    public static int readIntNumber() {
+        boolean repeat = true;
+        while (repeat) {
+
+            try {
+                int readNumber = sc.nextInt();
+                sc.nextLine();
+                repeat = false;
+                return readNumber;
+
+            } catch (InputMismatchException e) {
+                sc.nextLine();
+                System.out.println("Choose numeric vale only");
+                System.out.println("=====================");
+                System.out.print("Make choice again: ");
+            }
+        }
+        return -1;
+    }
 
 
 }
--- src/main/java/org/example/services/Main.java
+++ src/main/java/org/example/services/Main.java
@@ -1,19 +1,17 @@
 package org.example.services;
 
-import org.example.pojos.heroes.*;
-
 import static org.example.services.HeroManagementService.*;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        createMage();
-        createRanger();
-        createRogue();
-        createWarrior();
-        display();
-
+//        createMage();
+//        createRanger();
+//        createRogue();
+//        createWarrior();
+//        display();
+addHeroesMenu();
     }
 
 }
